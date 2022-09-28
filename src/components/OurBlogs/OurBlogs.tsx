2309823560edac78338297af6ebac413acd84f7a
mayuri-web <mayuriyawalkar.25@gmail.com>
2022-09-28 17:04:52 +0530

    [MY] our blogs section updated

--- src/components/OurBlogs/OurBlogs.tsx
+++ src/components/OurBlogs/OurBlogs.tsx
@@ -1,125 +1,88 @@
-import React, { Component } from 'react';
-import { Box, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
-import {withStyles} from '@mui/styles';
+import React, { Component } from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { withStyles } from "@mui/styles";
 
-import { BlogStyles } from './OurBlog.Style';
-import blog1 from '../../assets/images/Blogimages/blogimg-1.png';
-import blog2 from '../../assets/images/Blogimages/blogimg-2.png';
-import blog3 from '../../assets/images/Blogimages/blogimg-3.png';
+import { BlogStyles } from "./OurBlog.Style";
+import { blogsData } from "../../utils/data/blogs/blogs_data";
 
-interface blogProps{
-    classes: any;
+interface blogProps {
+  classes: any;
 }
 class OurBlogs extends Component<blogProps> {
-    render() {
-        const {classes} = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return (
-            <>
-                <Box sx={{mb:10}}>
-                    <Box>
-                        <Typography className={classes.blogHeading}>
-                            Our Blogs
+    return (
+      <>
+        <Box sx={{ mb: 10 }}>
+          <Box sx={{mb:4}}>
+            <Typography className={classes.blogHeading}>
+                Our Blogs
+            </Typography>
+            <Typography className={classes.blogTitleLine}></Typography>
+          </Box>
+          <Grid
+            container
+            justifyContent="center"
+            columnSpacing={4}
+            rowSpacing={3}
+            sx={{ px: { sm: 4, xs: 2, md: 0, lg: 0 } }}
+          >
+            {blogsData.map((item) => {
+              return (
+                <Grid item lg={3} md={3} xs={12} sm={6} key={item.id}>
+                  <Box className={classes.blogCard}>
+                    <Box sx={{ position: "relative" }}>
+                      <img
+                        height="230"
+                        width="100%"
+                        src={item.image}
+                        alt="blog 1"
+                      />
+                      <Box 
+                        sx={{
+                          width: "20%",
+                          position: "absolute",
+                          left: "40%",
+                          top: "78%",
+                          py: 1,
+                          px: 1,
+                          background: "#272522",
+                          color: "#E7A356",
+                          borderRadius: "5px",
+                          "&:hover": {
+                            background: "#E7A356",
+                            color: "#fff",
+                          },
+                        }}
+                      >
+                        <Typography className={classes.blogDate}>
+                          {item.date}
                         </Typography>
-                        <Typography sx={{width:'30px',borderBottom:'5px solid #EBB273'}}></Typography>
+                        <Typography className={classes.blogMonth}>
+                          {item.month}
+                        </Typography>
+                      </Box>
                     </Box>
-                    <Grid container justifyContent='center' columnSpacing={4}
-                    rowSpacing={3}
-                    >
-                        <Grid item lg={3} md={3} xs={12} sm={12}>
-                            <Card className={classes.blogCard} >
-                                <CardMedia
-                                component="img"
-                                height="200"
-                                image={blog1}
-                                alt="blog 1"
-                                />
-                                <CardContent>
-                                    <Box>
-                                        <Box 
-                                            sx={{
-                                                width:'20%',
-                                                position:'relative',
-                                                left:'40%',
-                                                top:'250%',
-                                                zIndex:1,
-                                                py:2,
-                                                px:2,
-                                                background:'#272522',
-                                                borderRadius:'5px',
-                                                // display:'flex',
-                                                // alignItems:'center',
-                                                // justifyContent:'center'
-                                            }}
-                                        >
-                                            <Typography sx={{textAlign:'center'}}>
-                                                25
-                                            </Typography>
-                                            <Typography sx={{textAlign:'center'}}>
-                                                June
-                                            </Typography>
-                                        </Box>
-                                        <Typography className={classes.blogCardTitle}>
-                                            Men Haircut new style
-                                        </Typography>
-                                        <Typography className={classes.blogCardText}>
-                                            Lorem ipsum dolor sit amet, 
-                                            consectetur adipiscing elit, 
-                                            sed do eiusmod tempor incididunt ut labore et
-                                        </Typography>
-                                    </Box>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                        <Grid item lg={3} md={3} xs={12} sm={12}>
-                            <Card className={classes.blogCard}>
-                                <CardMedia
-                                component="img"
-                                height="200"
-                                image={blog2}
-                                alt="blog 2"
-                                />
-                                <CardContent>
-                                    <Box>
-                                        <Typography className={classes.blogCardTitle}>
-                                         eyebrow shapes
-                                        </Typography>
-                                        <Typography className={classes.blogCardText}>
-                                            Lorem ipsum dolor sit amet, 
-                                            consectetur adipiscing elit, 
-                                            sed do eiusmod tempor incididunt ut labore et
-                                        </Typography>
-                                    </Box>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                        <Grid item lg={3} md={3} xs={12} sm={12}>
-                            <Card className={classes.blogCard}>
-                                <CardMedia
-                                component="img"
-                                height="200"
-                                image={blog3}
-                                alt="blog 3"
-                                />
-                                <CardContent>
-                                    <Box>
-                                        <Typography className={classes.blogCardTitle}>
-                                            Top 5 benefits facial
-                                        </Typography>
-                                        <Typography className={classes.blogCardText}>
-                                            Lorem ipsum dolor sit amet, 
-                                            consectetur adipiscing elit, 
-                                            sed do eiusmod tempor incididunt ut labore et
-                                        </Typography>
-                                    </Box>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </>
-        );
-    }
+                    <Typography className={classes.blogCardTitle}>
+                      {item.name}
+                    </Typography>
+                    <Typography className={classes.blogCardText}>
+                      {item.description}
+                    </Typography>
+                  </Box>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Box>
+      </>
+    );
+  }
 }
 
 export default withStyles(BlogStyles)(OurBlogs);
--- src/utils/models/navbar_interface.ts
+++ src/utils/models/navbar_interface.ts
@@ -6,6 +6,15 @@ export interface landingMenus{
     title: string;
 }
 
+export interface blogsdata{
+    id: number;
+    image:any;
+    name: string;
+    date: any;
+    month: any;
+    description: string;
+}
+
 export interface footerServices{
     id: number;
     path: string;
