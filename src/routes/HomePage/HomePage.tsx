d10f44513b944cfaeacb92f745932540c324d49e
Snehal Wakde <wakdesnehal12@gmail.com>
2022-09-28 11:38:55 +0530

    [SW] ourfeature section completed

--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -5,10 +5,11 @@ import TeamSection from './../../components/TeamSection/TeamSection';
 export default function HomePage() {
   return (
     <>
-      <div style={{height:"900px",backgroundColor:"red"}}>HomePage</div>
-      <div id="categories" style={{height:"900px",backgroundColor:"yellow"}}><Categories/></div>
-      <div id="teamsection" style={{height:"900px",backgroundColor:"blue"}}><TeamSection/></div>
-      <div id="features"><OurFeature/></div>
+    <div style={{height:"900px",backgroundColor:"red"}}>HomePage
+    </div>
+    <div id="services" style={{height:"900px",backgroundColor:"yellow"}}><Categories/></div>
+    <div id="teamsection" style={{height:"900px",backgroundColor:"blue"}}><TeamSection/></div>
+    <div id='features'><OurFeature/></div>
     </>
   )
 }
---
+++ src/utils/data/footer/footer_data.ts
@@ -0,0 +1,9 @@
+import { footerServices } from "../../models/navbar_interface";
+
+export const services:footerServices[] = [
+    {id:1, path:'#', title:'Haircut & Colour'},
+    {id:2, path:'#', title:'Nail'},
+    {id:3, path:'#', title:'Massage'},
+    {id:4, path:'#', title:'Beard'},
+    {id:5, path:'#', title:'Facial'},
+]
