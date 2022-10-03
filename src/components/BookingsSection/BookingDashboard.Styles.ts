d09a39e79e3e9ca7252ba33ea400ffa0fb9740fc
kiranrega <kiran.minpro@gmail.com>
2022-10-03 15:16:07 +0530

    [KR]-after adding cards of customers

--- src/components/BookingsSection/BookingDashboard.Styles.ts
+++ src/components/BookingsSection/BookingDashboard.Styles.ts
@@ -1,10 +1,12 @@
 export const Styles: any = (theme: any) => ({
   dashboardAvatar: {
-    width: "74px",
-    height: "74px",
     [theme.breakpoints.down("md")]: {
       width: "54px",
       height: "54px",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "74px !important",
+      height: "74px !important",
     },
   },
   customerCost: {
@@ -20,35 +22,51 @@ export const Styles: any = (theme: any) => ({
     },
   },
   customerName: {
-    fontFamily: "Fira Sans",
-    color: "#272522",
-    fontSize: { xs: "22px", md: "24px" },
-    fontWeight: "600",
-    lineHeight: "48px",
-    textTransform: "capitalize",
+    fontFamily: "Fira Sans !important",
+    color: "#272522 !important",
+
+    fontWeight: "600 !important",
+    lineHeight: "48px !important",
+    textTransform: "capitalize !important",
     fontStyle: "normal",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "22px !important",
+    },
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "22px !important",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "24px !important",
+    },
   },
   bookingsServicesTitle: {
-    fontFamily: "Fira Sans",
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: { md: "14px", lg: "16px" },
-    lineHeight: "36px",
+    fontFamily: "Fira Sans !important",
+    fontStyle: "normal !important",
+    fontWeight: " 500 !important",
+    lineHeight: "48px !important",
     textTransform: "capitalize",
-    color: "#88878F",
+    color: "#272522 !important",
+    [theme.breakpoints.down("lg")]: {
+      fontSize: "16px !important",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "18px !important",
+    },
   },
   bookingsSubHeading: {
-    fontWeight: "500",
-    fontSize: { xs: "16px", md: "18px" },
-    lineHeight: "28px",
-    color: "#272522",
+    fontWeight: "500 !important",
+    lineHeight: "28px !important",
+    color: "#272522 !important",
     fontStyle: "noraml",
-    fontFamily: "Fira Sans",
-    letterSpacing: "0.01",
-    textTransform: "capitalize",
-    // [theme.breakpoints.down("sm")]: {
-    //   fontSize: "16px !important",
-    // },
+    fontFamily: "Fira Sans !important",
+    letterSpacing: "0.01 !important",
+    textTransform: "capitalize !important",
+    [theme.breakpoints.up("xs")]: {
+      fontSize: "16px !important",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "18px !important",
+    },
   },
   bookingTimeHeading: {
     fontFamily: "Fira Sans",
@@ -87,5 +105,59 @@ export const Styles: any = (theme: any) => ({
     marginTop: "58px",
     marginBottom: "91px",
     mx: "auto",
+  },
+  serviceNameContainer: {
+    backgroundColor: "#F0F0F0",
+    // textAlign: "center",
+    borderRadius: "5px",
+    [theme.breakpoints.down("md")]: {
+      width: "87px",
+      height: "37px",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "112px",
+      height: "49px",
+    },
+  },
+  bookingsServicesName: {
+    fontFamily: "Fira Sans !important",
+    fontStyle: "normal !important",
+    fontWeight: "500 !important",
+
+    lineHeight: "36px !important",
+    textTransform: "capitalize",
+    color: "#88878F",
+    [theme.breakpoints.down("md")]: {
+      fontSize: "12px !important",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "14px !important",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "16px !important",
+    },
+  },
+  profileHeaderCost: {
+    fontFamily: "Fira Sans !important",
+    fontStyle: "normal !important",
+    fontWeight: "600 !important",
+    fontSize: "24px !important",
+    lineHeight: "48px !important",
+    textTransform: "capitalize !important",
+    color: "#88878F !important",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "22px !important",
+    },
+  },
+  customerContainer: {
+    maxWidth: "555px",
+    p: "20px",
+    height: "auto",
+    "&:hover": {
+      title: {
+        color: "#E7A356",
+      },
+    },
+    boxShadow: "0px 0px",
   },
 });
