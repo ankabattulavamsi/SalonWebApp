5ad6620bd3f1ba2a979ba0772b6a96b19b463d95
Snehal Wakde <wakdesnehal12@gmail.com>
2022-10-03 18:10:38 +0530

    [SW] adding category file

---
+++ src/components/Categories/SalonCategory.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@mui/material'
+import React from 'react'
+
+const SalonCategory = () => {
+  return (
+    <>
+      <Container style={{margin: "100px 0", paddingTop: "39px"}}>
+        <h1>Hello world</h1>
+      </Container>
+    </>
+  )
+}
+
+export default SalonCategory;
--- src/routes/SalonPartner/SalonPartner.tsx
+++ src/routes/SalonPartner/SalonPartner.tsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 import Footer from "../../components/common/Footer/Footer";
 import Navbar from "../../components/common/Navbar/Navbar";
 import SalonNavbar from "../../components/common/Navbar/SalonNavbar";
+import SalonCategory from "../../components/Categories/SalonCategory";
+
 
 
 class SalonPartner extends Component{
@@ -11,6 +13,7 @@ class SalonPartner extends Component{
     return (
       <Fragment>
         <SalonNavbar />
+        <SalonCategory/>
         <Outlet />
         {/* <Footer /> */}
       </Fragment>
