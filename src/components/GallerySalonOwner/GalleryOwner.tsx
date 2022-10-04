8b758ffe2b03a2a17a31dfad48a72998012126cf
Shubham Sarode <shubham.sarode@extwebtech.in>
2022-10-04 13:05:57 +0530

    completed gallery section

--- src/components/GallerySalonOwner/GalleryOwner.tsx
+++ src/components/GallerySalonOwner/GalleryOwner.tsx
@@ -11,12 +11,14 @@ import {
 import { withStyles } from "@mui/styles";
 import { Box } from "@mui/system";
 import React, { Component } from "react";
+import { Edit, Delete } from "@mui/icons-material";
 import {
   gallaryData,
   GallaryData,
 } from "../../utils/data/GalleryOwner/Gallery";
 import ViewAll from "../common/ViewBtn/ViewAll";
 import { galleryStyles } from "./GalleryOwners.styles";
+import "./GalleryOwner.styles.css";
 interface GalleryOwnerProps {
   classes: any;
 }
@@ -44,7 +46,10 @@ class GalleryOwner extends Component<GalleryOwnerProps, GalleryOwnerState> {
             {gallaryData.map((images: GallaryData) => {
               return (
                 <Grid item xs={12} sm={6} md={4} key={images.id}>
-                  <Card sx={{ width: "100%" }} className={classes.card}>
+                  <Card
+                    sx={{ width: "100%" }}
+                    className={`cards ${classes.card}`}
+                  >
                     <CardMedia
                       component="img"
                       alt={`${images.id}`}
@@ -54,9 +59,22 @@ class GalleryOwner extends Component<GalleryOwnerProps, GalleryOwnerState> {
                       className={classes.imgCard}
                     />
 
-                    <CardActions className={classes.cardActions}>
-                      <Button>edit</Button>
-                      <Button>delete</Button>
+                    <CardActions className={`cardActio ${classes.cardActions}`}>
+                      <Box
+                        sx={{
+                          width: "100%",
+                          mx: "auto",
+                          justifyContent: "center",
+                          gap: "50px",
+                        }}
+                      >
+                        <Button className={classes.buttonClass}>
+                          <Edit />
+                        </Button>
+                        <Button className={classes.buttonClass}>
+                          <Delete />
+                        </Button>
+                      </Box>
                     </CardActions>
                   </Card>
                 </Grid>
--- src/components/GallerySalonOwner/GalleryOwners.styles.ts
+++ src/components/GallerySalonOwner/GalleryOwners.styles.ts
@@ -23,17 +23,39 @@ export const galleryStyles: any = (theme: any) => ({
     position: "relative",
     width: "100%",
     height: "100%",
+    borderRadius: "4px !important",
+    transition: " all .5s",
   },
-  imgCard: {
-    "&:hover": {
-      opacity: 0.6,
-    },
-  },
+  imgCard: {},
 
   cardActions: {
     position: "absolute",
+    transition: " all .5s",
+
+    display: "none !important",
     top: "50%",
     left: "50%",
-    display: "none",
+    transform: "translate(-50%, -50%)",
+    background: "linear-gradient(45deg,rgba(0,0,0,.6),rgba(0,0,0,.3))",
+    width: "100%",
+    height: "100%",
+    textAlign: "center",
+  },
+
+  buttonClass: {
+    border: "2px solid #ffffff !important",
+    marginLeft: "3px !important",
+    marginRight: "3px !important",
+    padding: "10px !important",
+    color: "#ffffff !important",
+    background: "transparent",
+    transition: " all .5s",
+
+    "&:hover": {
+      background: "#E7A356 !important",
+      transition: " all .5s",
+
+      border: "2px solid #E7A356 !important",
+    },
   },
 });
