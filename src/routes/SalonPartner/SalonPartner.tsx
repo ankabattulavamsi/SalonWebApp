c3d415102fd2d8cd08adbd96d36541b5f750ba48
Shubham Sarode <shubham.sarode@extwebtech.in>
2022-10-04 13:14:26 +0530

    [SS] change some name of interface

--- src/routes/SalonPartner/SalonPartner.tsx
+++ src/routes/SalonPartner/SalonPartner.tsx
@@ -4,13 +4,13 @@ import { Outlet } from "react-router-dom";
 import Footer from "../../components/common/Footer/Footer";
 import SalonNavbar from "../../components/common/Navbar/SalonNavbar";
 import GalleryOwner from "../../components/GallerySalonOwner/GalleryOwner";
-import { SalonMenu, CustomerMenu } from "../../utils/data/navbar_menus";
+import { salonMenu, CustomerMenu } from "../../utils/data/navbar_menus";
 
 class SalonPartner extends Component {
   render() {
     return (
       <Fragment>
-        <SalonNavbar link="Home" customer={false} menus={SalonMenu} />
+        <SalonNavbar link="Home" customer={false} menus={salonMenu} />
         <Outlet />
         <div id="/" style={{ marginTop: "100px" }}>
           <GalleryOwner />
