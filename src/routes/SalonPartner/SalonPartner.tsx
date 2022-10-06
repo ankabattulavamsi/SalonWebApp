1b9c11a19b73b9b391602639c0ed6276ac0690ca
kiranrega <kiran.minpro@gmail.com>
2022-10-06 18:38:47 +0530

    [KR]-merge from develop branch

--- src/routes/SalonPartner/SalonPartner.tsx
+++ src/routes/SalonPartner/SalonPartner.tsx
@@ -4,24 +4,18 @@ import { Grid } from "@mui/material";
 import Footer from "../../components/common/Footer/Footer";
 import SalonNavbar from "../../components/common/Navbar/SalonNavbar";
 import { SalonMenu } from "../../utils/data/navbar_menus";
+import BookingDashboard from "../../components/BookingsSection/BookingDashboard";
 
-class SalonPartner extends Component{
-
+class SalonPartner extends Component {
   render() {
     return (
       <Fragment>
-        <SalonNavbar 
-          link="Home"
-          customer={false}
-          menus={SalonMenu}
-        />
+        <SalonNavbar link="Home" customer={false} menus={SalonMenu} />
         <Grid container>
           {/* salon page sections */}
-          
+          <BookingDashboard />
         </Grid>
-        <Footer 
-          salon={true}
-        />
+        <Footer salon={true} />
       </Fragment>
     );
   }
