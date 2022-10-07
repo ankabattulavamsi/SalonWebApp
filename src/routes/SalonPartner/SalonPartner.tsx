eee3bd9526c2b47d7b44f44f2fc5d51a87c55791
Mohammed Umair <94831461+Mohammed-Umair@users.noreply.github.com>
2022-10-07 16:05:07 +0530

    [MU] update code in another branch

--- src/routes/SalonPartner/SalonPartner.tsx
+++ src/routes/SalonPartner/SalonPartner.tsx
@@ -1,29 +1,30 @@
 import { Box } from "@mui/system";
 import React, { Component, Fragment } from "react";
-import { Grid } from "@mui/material";
 
-import Footer from "../../components/common/Footer/Footer";
 import SalonNavbar from "../../components/common/Navbar/SalonNavbar";
 import SalonPatnerBestOffers from "../../components/OffersSection/SalonPatnerBestOffers";
-import { salonMenu, CustomerMenu } from "../../utils/data/navbar_menus";
-import { Outlet } from "react-router-dom";
+import { salonMenu } from "../../utils/data/navbar_menus";
+import Specialists from "../../components/OurSpecialists/Specialists";
+import Footer from "../../components/common/Footer/Footer";
+import OwnerDashboardCharts from "../../components/OwnerDashboardChart/OwnerDashboardCharts";
+import { Grid } from "@mui/material";
 
-class SalonPartner extends Component{
-
+class SalonPartner extends Component {
   render() {
     return (
       <Fragment>
-        <SalonNavbar 
-          link="Home"
-          customer={false}
-          menus={salonMenu}
-        />
-        <Outlet />
-        <Box>
-        <SalonPatnerBestOffers />
-        </Box>
-        {/* <Footer /> */}
-      </Fragment>
+				<SalonNavbar link="Home" customer={false} menus={salonMenu} />
+				<Grid container>
+					{/* salon page sections */}
+					<OwnerDashboardCharts />
+				</Grid>
+				<Box sx={{ mt: 10 }}>
+					<SalonPatnerBestOffers />
+				</Box>
+				<Specialists />
+				<Footer salon={true} />
+			
+			</Fragment>
     );
   }
 }
