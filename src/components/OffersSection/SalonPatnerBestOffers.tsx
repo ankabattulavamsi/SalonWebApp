616f268c77f88396fbbeeec6b4d5fcaf5ceceeeb
Vamsi Ankabattula <vamsiankabattula97@gmail.com>
2022-10-10 19:02:03 +0530

    [VA] mergin

--- src/components/OffersSection/SalonPatnerBestOffers.tsx
+++ src/components/OffersSection/SalonPatnerBestOffers.tsx
@@ -59,7 +59,7 @@ export class SalonPatnerBestOffers extends Component<IsStateProps> {
     return (
       <>
         <Box>
-          <Box sx={{ pt: 5, pb: 5, mt: 5 }} className={classes.MainContainer}>
+          <Box sx={{ pt: 11, pb: 11, mt: 5 }} className={classes.MainContainer}>
             <Grid
               container
               justifyContent="center"
