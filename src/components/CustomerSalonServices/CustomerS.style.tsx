658e6b80779fc13ed7756baf93971cfd806f965a
Vamsi Ankabattula <vamsiankabattula97@gmail.com>
2022-10-27 12:41:00 +0530

    [VA] updated single page

--- src/components/CustomerSalonServices/CustomerS.style.tsx
+++ src/components/CustomerSalonServices/CustomerS.style.tsx
@@ -1,7 +1,21 @@
+import { lineHeight } from "@mui/system";
+
 export const hairStyle: any = {
   priceServeContainer: {
     display: "flex",
     justifyContent: "space-between",
+  },
+  headContainer : {
+    paddingTop: '8%',
+    "@media screen and (max-width: 600px)": {
+      paddingTop: '45%',
+    },
+    "@media screen and (min-width: 600px) and (max-width: 1080px)": {
+      paddingTop: '22%',
+    },
+    "@media screen and (min-width: 912px) and (max-width: 1370px)": {
+      paddingTop: '10%',
+    },
   },
   priceServeContainerR: {
     display: "flex",
@@ -69,9 +83,6 @@ export const hairStyle: any = {
       color: '#272522  !important', 
       transitionDelay: '0.2s !important',
     },
-    // "&:hover div div div div svg:nth-child(2)": {
-    //   color: '#E7A356 !important'
-    // },
     "&:hover div button": {
       background: "#E7A356 !important",
       color: "#FFFFFF !important",
@@ -88,9 +99,9 @@ export const hairStyle: any = {
   MainHeading: {
     fontFamily: "Fira Sans",
     fontStyle: "SemiBold",
-    fontWeight: "700",
+    fontWeight: "700 !important",
     fontSize: "26px !important",
-    lineHeight: "48px",
+    lineHeight: "48px !important",
     color: "#272522 !important",
     "@media screen and (max-width: 600px)": {
       fontSize: "26px !important",
@@ -100,12 +111,67 @@ export const hairStyle: any = {
     fontFamily: "Roboto",
     fontStyle: "Regular",
     fontWeight: "700",
-    marginTop: "8px !important",
+    marginTop: "4px !important",
     fontSize: "17px !important",
-    lineHeight: "26px",
+    lineHeight: "27px !important",
     color: "#88878F !important",
     "@media screen and (max-width: 600px)": {
       fontSize: "18px !important",
     },
+  }, 
+  descriptionW: {
+    fontFamily: "Roboto",
+    fontStyle: "Regular",
+    fontWeight: "700",
+    marginTop: "12px !important",
+    fontSize: "17px !important",
+    lineHeight: "27px !important",
+    color: "#88878F !important",
+    
+    "@media screen and (max-width: 600px)": {
+      fontSize: "18px !important",
+    },
+  },
+  AddBtn: {
+    backgroundColor: "#E7A356 !important",
+    borderRadius: "8px !important",
+    color: "#FFF !important",
+    width: "261px",
+    height: "62px",
+    fontSize: '18px !important',
+    lineHeight: '22px',
+    fontFamily: "Fira Sans",
+    fontStyle: "SemiBold",
+    letteSpacing: '2% !important',
+    marginTop: "12px !important",
+    marginBottom: '10% !important',
+    "@media screen and (max-width: 600px)": {
+      width: "100%",
+      marginBottom: '10% !important',
+    },
+  },
+  priceContianer: {
+    display: 'flex',
+    '& h3':{
+      fontFamily: 'Fira Sans',
+      fontStyle: 'medium',
+      fontWeight: '700',
+      fontSize: '18px',
+      lineHeight: '48px',
+      color: "#E7A356 !important",
+  },
+  '& h2':{
+    fontFamily: 'Fira Sans',
+    fontStyle: 'medium',
+    fontWeight: '700',
+    fontSize: '18px',
+    lineHeight: '48px',
+    color: "#272522",
+    textDecoration: "line-through",
+},
+  },
+  priceContianerS: {
+    display: 'flex',
+    alignItems: 'center'
   }
 };
--- src/components/CustomerSalonServices/CustomerSingleServicePage.tsx
+++ src/components/CustomerSalonServices/CustomerSingleServicePage.tsx
@@ -4,38 +4,71 @@ import { Box, Container } from "@mui/system";
 import React, { Component } from "react";
 import WithRouterHoc from "../common/CommonNavigateComp/WithRouterHoc";
 import Layout from "../Layout/Layout";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 import { hairStyle } from "./CustomerS.style";
 interface IsSinglePageProps {
   location: any;
-  classes:any
+  classes: any;
 }
 
 class CustomerSingleServicePage extends Component<IsSinglePageProps> {
   render() {
     const { state } = this.props.location;
-    const {classes} = this.props
+    const { classes } = this.props;
     console.log("location-data", state);
     return (
       <Layout customer={true}>
-        <Container sx={{ mt: 20, mb: 10 }}>
-          <Grid container spacing={2} sx={{ p: { lg: 0 } }}>
+        <Container className={classes.headContainer}>
+          <Grid container spacing={4} sx={{ p: { lg: 0 } }}>
             <Grid item xs={12} md={6} sm={6} lg={6}>
               <Box>
-                <img src={state.hairServeImg} alt="hair" style={{width: '500px'}} />
+                <img
+                  src={state.hairServeImg}
+                  alt="hair"
+                  style={{ width: "250px" }}
+                />
               </Box>
             </Grid>
 
             <Grid item xs={12} md={6} sm={6} lg={6}>
-            <Typography className={classes.MainHeading}>{state.heading}</Typography>
-              <Box>
-                <Typography>{state.price}</Typography>
-                <Typography >{state.dissPrice}</Typography>
+              <Typography className={classes.MainHeading}>
+                {state.heading}
+              </Typography>
+              <Box className={classes.priceContianer}>
+                <Box className={classes.priceContianerS}>
+                  <CurrencyRupeeIcon
+                    style={{ fontSize: "16px", color: "#E7A356" }}
+                  />
+                  <Typography variant="h3">{state.price}</Typography>
+                </Box>
+
+                <Box className={classes.priceContianerS}>
+                  <CurrencyRupeeIcon
+                    style={{ fontSize: "16px", marginLeft: "8px" }}
+                  />
+                  <Typography variant="h2">{state.dissPrice}</Typography>
+                </Box>
               </Box>
-              <Typography className={classes.descriptionP}>{state.description}</Typography>
-            <Typography className={classes.descriptionP}>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking
-            </Typography>
-              <Button>Add To Cart</Button>
+              <Typography className={classes.descriptionP}>
+                {state.description}
+              </Typography>
+              <Typography className={classes.descriptionW}>
+                On the other hand, we denounce with righteous indignation and
+                dislike men who are so beguiled and demoralized by the charms of
+                pleasure of the moment, so blinded by desire, that they cannot
+                foresee the pain and trouble
+              </Typography>
+              <Button
+                className={classes.AddBtn}
+                startIcon={
+                  <ShoppingBasketIcon
+                    style={{ fontSize: "26px", marginBottom: "5px" }}
+                  />
+                }
+              >
+                Add To Cart
+              </Button>
             </Grid>
           </Grid>
         </Container>
