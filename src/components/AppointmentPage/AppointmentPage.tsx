b912d66c0758e27430d12542a345f5e0d8306dee
Rajnish Kalwar <rajnish.k@extwebtech.in>
2022-11-01 13:07:40 +0530

    [RK] payment method is added

--- src/components/AppointmentPage/AppointmentPage.tsx
+++ src/components/AppointmentPage/AppointmentPage.tsx
@@ -1,250 +1,286 @@
+/** @format */
+
 import {
-  Button,
-  Container,
-  FormControl,
-  MenuItem,
-  Select,
-  Typography,
+	Button,
+	Container,
+	FormControl,
+	MenuItem,
+	Select,
+	Typography,
 } from "@mui/material";
 import { withStyles } from "@mui/styles";
 import { Box } from "@mui/system";
 import React, { Component } from "react";
 import BookingsCalendarComponent from "../BookingsSection/BookingsCalendarComponent/BookingsCalendarComponent";
 import {
-  timingDataMorning,
-  timingDataAfternoon,
-  timingDataEvening,
-} from "../../utils/data/CustomerAppointment/TimingsData";
+	timingDataMorning,
+	timingDataAfternoon,
+	timingDataEvening,
+} from "../../utils/data/CustomerAppointment/salonEmployeeData";
 import { Styles } from "./AppointmentPage.Style";
 import Layout from "../Layout/Layout";
 import { salonEmpData } from "../../utils/data/CustomerAppointment/salonEmployeeData";
-import { time } from "console";
+import CustomerPaymentDetail from "../common/CustomerPaymentDetail/CustomerPaymentDetail";
 
 class CustomerAppointmentPage extends Component {
-  state = { monthName: "", isActiveTime: "01:00 PM", salonEmpSelected: 4 };
-  handleMonthChange = (event: any) => {
-    this.setState({ monthName: event.target.value });
-  };
-  salonEmpActive = (index: number) => {
-    this.setState({ salonEmpSelected: index });
-  };
+	state = {
+		monthName: "",
+		isActiveTime: "01:00 PM",
+		salonEmpSelected: 4,
+		open: false,
+		bankAccount: true,
+		upi: false,
+		creditCard: false,
+	};
+	handleMonthChange = (event: any) => {
+		this.setState({ monthName: event.target.value });
+	};
+	salonEmpActive = (index: number) => {
+		this.setState({ salonEmpSelected: index });
+	};
 
-  onclickActive = (time: string) => {
-    this.setState({ isActiveTime: time });
-  };
-  render() {
-    const { classes }: any = this.props;
-    return (
-      <Layout customer={true}>
-        <Container maxWidth="lg" sx={{ py: 5, mt: { xs: 10, md: 4 } }}>
-          <Box className={classes.AppointmentHeadingContainer}>
-            <Typography variant="h6" className={classes.scheduleAppointment}>
-              schedule your appointment
-            </Typography>
-            <FormControl>
-              <Select
-                labelId="monthName"
-                id="monthName"
-                value={this.state.monthName}
-                onChange={this.handleMonthChange}
-                name="monthName"
-                className={classes.monthName}
-                displayEmpty
-              >
-                <MenuItem value="">January</MenuItem>
-                <MenuItem value="february">February</MenuItem>
-                <MenuItem value="March">March</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-          <Box
-            sx={{
-              boxShadow: "2px 2px 2px 2px #F0F0F0",
-              mt: 3,
-              borderRadius: "10px",
-              width: "100%",
-            }}
-          >
-            <BookingsCalendarComponent />
-          </Box>
-          <Box
-            sx={{
-              mt: 3,
-              ml: 2,
-              p: 4,
-              backgroundColor: "#F8F8F8",
-              width: "100%",
-              borderRadius: "10px",
-            }}
-            className={classes.scroller}
-          >
-            <Typography
-              variant="h6"
-              className={classes.availableSlot}
-              sx={{ mx: 2 }}
-            >
-              Available Slot
-            </Typography>
-            <Box
-              className={classes.AppointmentBox}
-              sx={{ display: "flex", py: 2 }}
-            >
-              <Typography
-                variant="h6"
-                sx={{ mr: 5 }}
-                className={classes.appointmentTimeTitle}
-              >
-                Morning
-              </Typography>
-              {timingDataMorning.map((item: any) => (
-                <Button
-                  sx={{
-                    borderRadius: "10px !important",
-                  }}
-                  onClick={() => this.onclickActive(item.time)}
-                >
-                  <Typography className={classes.appointmentTime}>
-                    {item.time}
-                  </Typography>
-                </Button>
-              ))}
-            </Box>
+	onclickActive = (time: string) => {
+		this.setState({ isActiveTime: time });
+	};
 
-            <Box
-              className={classes.AppointmentBox}
-              sx={{ display: "flex", py: 2 }}
-            >
-              <Typography
-                variant="h6"
-                sx={{ mr: 3 }}
-                className={classes.appointmentTimeTitle}
-              >
-                Afternoon
-              </Typography>
-              {timingDataAfternoon.map((item: any) => (
-                <Button
-                  sx={{
-                    borderRadius: "10px !important",
-                  }}
-                  onClick={() => this.onclickActive(item.time)}
-                  className={classes.dateTime}
-                >
-                  <Box>
-                    <Typography className={classes.appointmentTime}>
-                      {item.time}
-                    </Typography>
-                  </Box>
-                </Button>
-              ))}
-            </Box>
+	handleClose = () => {
+		this.setState({
+			open: false,
+		});
+	};
+	handleOpenModel = () => {
+		this.setState({
+			open: true,
+		});
+	};
+	handleBankAccPayment = () => {
+		this.setState({
+			bankAccount: true,
+			upi: false,
+			creditCard: false,
+		});
+	};
+	handleUPIPayment = () => {
+		this.setState({
+			upi: true,
+			bankAccount: false,
+			creditCard: false,
+		});
+	};
+	handleCreditCardPayment = () => {
+		this.setState({
+			creditCard: true,
+			upi: false,
+			bankAccount: false,
+		});
+	};
+	render() {
+		const { classes }: any = this.props;
+		return (
+			<Layout customer={true}>
+				<Container maxWidth="lg" sx={{ py: 5, mt: { xs: 10, md: 4 } }}>
+					<Box className={classes.AppointmentHeadingContainer}>
+						<Typography
+							variant="h6"
+							className={classes.scheduleAppointment}>
+							schedule your appointment
+						</Typography>
+						<FormControl>
+							<Select
+								labelId="monthName"
+								id="monthName"
+								value={this.state.monthName}
+								onChange={this.handleMonthChange}
+								name="monthName"
+								className={classes.monthName}
+								displayEmpty>
+								<MenuItem value="">January</MenuItem>
+								<MenuItem value="february">February</MenuItem>
+								<MenuItem value="March">March</MenuItem>
+							</Select>
+						</FormControl>
+					</Box>
+					<Box
+						sx={{
+							boxShadow: "2px 2px 2px 2px #F0F0F0",
+							mt: 3,
+							borderRadius: "10px",
+							width: "100%",
+						}}>
+						<BookingsCalendarComponent />
+					</Box>
+					<Box
+						sx={{
+							mt: 3,
+							ml: 2,
+							p: 4,
+							backgroundColor: "#F8F8F8",
+							width: "100%",
+							borderRadius: "10px",
+						}}
+						className={classes.scroller}>
+						<Typography
+							variant="h6"
+							className={classes.availableSlot}
+							sx={{ mx: 2 }}>
+							Available Slot
+						</Typography>
+						<Box
+							className={classes.AppointmentBox}
+							sx={{ display: "flex", py: 2 }}>
+							<Typography
+								variant="h6"
+								sx={{ mr: 5 }}
+								className={classes.appointmentTimeTitle}>
+								Morning
+							</Typography>
+							{timingDataMorning.map((item: any) => (
+								<Button
+									sx={{
+										borderRadius: "10px !important",
+									}}
+									onClick={() => this.onclickActive(item.time)}>
+									<Typography className={classes.appointmentTime}>
+										{item.time}
+									</Typography>
+								</Button>
+							))}
+						</Box>
 
-            <Box
-              className={classes.AppointmentBox}
-              sx={{ display: "flex", py: 2 }}
-            >
-              <Typography
-                variant="h6"
-                sx={{ mr: 6 }}
-                className={classes.appointmentTimeTitle}
-              >
-                Evening
-              </Typography>
-              {timingDataEvening.map((item: any) => (
-                <Button
-                  sx={{
-                    borderRadius: "10px !important",
-                  }}
-                >
-                  <Typography className={classes.appointmentTime}>
-                    {item.time}
-                  </Typography>
-                </Button>
-              ))}
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              backgroundColor: "#F8F8F8",
-              borderRadius: "10px",
-              p: 5,
-              mt: 3,
-            }}
-          >
-            <Typography variant="h4" className={classes.selectYourStylish}>
-              Select Your Stylish
-            </Typography>
-            <Box
-              sx={{
-                display: "flex",
-                overflowY: "scroll",
-              }}
-            >
-              {salonEmpData.map((emp: any, index: number) => {
-                return (
-                  <Button
-                    onClick={() => this.salonEmpActive(index)}
-                    key={index}
-                    sx={{
-                      width: { sm: "100px", md: "76px" },
-                      height: "100px",
-                      m: 2,
-                    }}
-                  >
-                    <Box sx={{ m: 1 }}>
-                      <img
-                        src={emp.img}
-                        alt="name"
-                        width="76px"
-                        height="76px"
-                        style={{
-                          border:
-                            index === this.state.salonEmpSelected
-                              ? "4px solid #E7A356"
-                              : "",
-                          borderRadius: "12px",
-                        }}
-                      />
-                      <Typography
-                        variant="h6"
-                        className={classes.employeeName}
-                        sx={{
-                          color:
-                            index === this.state.salonEmpSelected
-                              ? "#E7A356"
-                              : "#88878F",
-                        }}
-                      >
-                        {emp.name}
-                      </Typography>
-                    </Box>
-                  </Button>
-                );
-              })}
-            </Box>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "flex-end",
-              minWidth: "100px",
-            }}
-          >
-            <Button
-              variant="contained"
-              className={classes.continueButton}
-              sx={{
-                "&.MuiButtonBase-root:hover": {
-                  bgcolor: "#E7A356",
-                },
-              }}
-            >
-              Continue
-            </Button>
-          </Box>
-        </Container>
-      </Layout>
-    );
-  }
+						<Box
+							className={classes.AppointmentBox}
+							sx={{ display: "flex", py: 2 }}>
+							<Typography
+								variant="h6"
+								sx={{ mr: 3 }}
+								className={classes.appointmentTimeTitle}>
+								Afternoon
+							</Typography>
+							{timingDataAfternoon.map((item: any) => (
+								<Button
+									sx={{
+										borderRadius: "10px !important",
+									}}
+									onClick={() => this.onclickActive(item.time)}
+									className={classes.dateTime}>
+									<Box>
+										<Typography className={classes.appointmentTime}>
+											{item.time}
+										</Typography>
+									</Box>
+								</Button>
+							))}
+						</Box>
+
+						<Box
+							className={classes.AppointmentBox}
+							sx={{ display: "flex", py: 2 }}>
+							<Typography
+								variant="h6"
+								sx={{ mr: 6 }}
+								className={classes.appointmentTimeTitle}>
+								Evening
+							</Typography>
+							{timingDataEvening.map((item: any) => (
+								<Button
+									sx={{
+										borderRadius: "10px !important",
+									}}>
+									<Typography className={classes.appointmentTime}>
+										{item.time}
+									</Typography>
+								</Button>
+							))}
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							backgroundColor: "#F8F8F8",
+							borderRadius: "10px",
+							p: 5,
+							mt: 3,
+						}}>
+						<Typography variant="h4" className={classes.selectYourStylish}>
+							Select Your Stylish
+						</Typography>
+						<Box
+							sx={{
+								display: "flex",
+								overflowY: "scroll",
+							}}>
+							{salonEmpData.map((emp: any, index: number) => {
+								return (
+									<Button
+										onClick={() => this.salonEmpActive(index)}
+										key={index}
+										sx={{
+											width: { sm: "100px", md: "76px" },
+											height: "100px",
+											m: 2,
+										}}>
+										<Box sx={{ m: 1 }}>
+											<img
+												src={emp.img}
+												alt="name"
+												width="76px"
+												height="76px"
+												style={{
+													border:
+														index === this.state.salonEmpSelected
+															? "4px solid #E7A356"
+															: "",
+													borderRadius: "12px",
+												}}
+											/>
+											<Typography
+												variant="h6"
+												className={classes.employeeName}
+												sx={{
+													color:
+														index === this.state.salonEmpSelected
+															? "#E7A356"
+															: "#88878F",
+												}}>
+												{emp.name}
+											</Typography>
+										</Box>
+									</Button>
+								);
+							})}
+						</Box>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "flex-end",
+							minWidth: "100px",
+						}}>
+						<Button
+							variant="contained"
+							onClick={this.handleOpenModel}
+							className={classes.continueButton}
+							sx={{
+								"&.MuiButtonBase-root:hover": {
+									bgcolor: "#E7A356",
+								},
+							}}>
+							Continue
+						</Button>
+					</Box>
+					<CustomerPaymentDetail
+						// open={this.state.open}
+						onClose={this.handleClose}
+						handleBankAccPayment={this.handleBankAccPayment}
+						handleUPIPayment={this.handleUPIPayment}
+						handleCreditCardPayment={this.handleCreditCardPayment}
+						state={this.state}
+					/>
+				</Container>
+			</Layout>
+		);
+	}
 }
 
-export default withStyles(Styles, { withTheme: true })(CustomerAppointmentPage);
+export default withStyles(Styles, { withTheme: true })(
+	CustomerAppointmentPage
+);
--- src/utils/data/CustomerAppointment/TimingsData.tsx
+++ src/utils/data/CustomerAppointment/TimingsData.tsx
@@ -1,16 +1,22 @@
-export const timingDataMorning = [{time: '10:00 AM'}, {time: '11:00 AM'}, {time: '11:30 AM'}];
+/** @format */
+
+export const timingDataMorning = [
+	{ time: "10:00 AM" },
+	{ time: "11:00 AM" },
+	{ time: "11:30 AM" },
+];
 export const timingDataAfternoon = [
-    {time: '12:00 PM'},
-    {time: '01:00 PM'},
-    {time: '02:00 PM'},,
-    {time: '03:00 PM'},,
-    {time: '03:30 PM'},,
-    {time: '04:00 PM'},,
+	{ time: "12:00 PM" },
+	{ time: "01:00 PM" },
+	{ time: "02:00 PM" },
+	{ time: "03:00 PM" },
+	{ time: "03:30 PM" },
+	{ time: "04:00 PM" },
 ];
 export const timingDataEvening = [
-    {time: '05:00 PM'},,
-    {time: '05:30 PM'},,
-    {time: '06:00 PM'},,
-    {time: '07:00 PM'},,
-    {time: '07:00 PM'},,
+	{ time: "05:00 PM" },
+	{ time: "05:30 PM" },
+	{ time: "06:00 PM" },
+	{ time: "07:00 PM" },
+	{ time: "07:00 PM" },
 ];
