38f8f9bd10a7cce4879dae1f4156e959eb719862
Vamsi Ankabattula <vamsiankabattula97@gmail.com>
2022-11-02 13:38:10 +0530

    [VA] updated

--- src/utils/data/CustomerAppointment/TimingsData.tsx
+++ src/utils/data/CustomerAppointment/TimingsData.tsx
@@ -1,34 +1,22 @@
 /** @format */
 
-export const timingsData = [
-  {
-    heading: "Morning",
-    timings: [{ time: "10:00 AM" }, { time: "11:00 AM" }, { time: "11:30 AM" }],
-  },
-  {
-    heading: "Afternoon",
-    timings: [
-      { time: "12:00 PM" },
-      { time: "01:00 PM" },
-      { time: "02:00 PM" },
-      { time: "03:00 PM" },
-      { time: "03:30 PM" },
-      { time: "04:00 PM" },
-    ],
-  },
+export const timingDataMorning = [
+	{ time: "10:00 AM" },
+	{ time: "11:00 AM" },
+	{ time: "11:30 AM" },
 ];
-// export const timingDataAfternoon = [
-//   { time: "12:00 PM" },
-//   { time: "01:00 PM" },
-//   { time: "02:00 PM" },
-//   { time: "03:00 PM" },
-//   { time: "03:30 PM" },
-//   { time: "04:00 PM" },
-// ];
+export const timingDataAfternoon = [
+	{ time: "12:00 PM" },
+	{ time: "01:00 PM" },
+	{ time: "02:00 PM" },
+	{ time: "03:00 PM" },
+	{ time: "03:30 PM" },
+	{ time: "04:00 PM" },
+];
 export const timingDataEvening = [
-  { time: "05:00 PM" },
-  { time: "05:30 PM" },
-  { time: "06:00 PM" },
-  { time: "07:00 PM" },
-  { time: "07:00 PM" },
+	{ time: "05:00 PM" },
+	{ time: "05:30 PM" },
+	{ time: "06:00 PM" },
+	{ time: "07:00 PM" },
+	{ time: "07:00 PM" },
 ];
