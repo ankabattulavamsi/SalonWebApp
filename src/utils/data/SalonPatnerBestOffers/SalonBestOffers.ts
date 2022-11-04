4e97d80c95972443e37d70c1b6145986e7e202f2
kiranrega <kiran.minpro@gmail.com>
2022-11-04 18:34:49 +0530

    Fixed bestoffers space in salon home

--- src/utils/data/SalonPatnerBestOffers/SalonBestOffers.ts
+++ src/utils/data/SalonPatnerBestOffers/SalonBestOffers.ts
@@ -1,21 +1,23 @@
-import offerImg1 from '../../../assets/images/SalonPatnerBestOffersImg/Rectangle64(1).png'
-import offerImg2 from '../../../assets/images/SalonPatnerBestOffersImg/Rectangle64.png'
+import offerImg1 from "../../../assets/images/SalonPatnerBestOffersImg/Rectangle64(1).png";
+import offerImg2 from "../../../assets/images/SalonPatnerBestOffersImg/Rectangle64.png";
 
-export const SalonBestOffersData:any = [
-    {
-        id: 1,
-        offerImage: offerImg1, 
-        headingOff: 'Hair Color 20% Off',
-        price: '240',
-        dissPrice: '150', 
-        description: 'it is a long established fact that a reader will be distracted by the readable content of a page when looking', 
-    }, 
-    {
-        id: 2,
-        offerImage: offerImg2, 
-        headingOff: 'Hair Color 20% Off',
-        price: '240',
-        dissPrice: '150', 
-        description: 'it is a long established fact that a reader will be distracted by the readable content of a page when looking', 
-    }, 
-] 
+export const SalonBestOffersData: any = [
+  {
+    id: 1,
+    offerImage: offerImg2,
+    headingOff: "Hair Color 20% Off",
+    price: "240",
+    dissPrice: "150",
+    description:
+      "it is a long established fact that a reader will be distracted by the readable content of a page when looking",
+  },
+  {
+    id: 2,
+    offerImage: offerImg1,
+    headingOff: "Hair Color 20% Off",
+    price: "240",
+    dissPrice: "150",
+    description:
+      "it is a long established fact that a reader will be distracted by the readable content of a page when looking",
+  },
+];
