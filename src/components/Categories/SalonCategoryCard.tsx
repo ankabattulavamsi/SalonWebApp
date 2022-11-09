986d6950bc0517737607052907fc878d99ffd67e
Mohammed Umair <94831461+Mohammed-Umair@users.noreply.github.com>
2022-11-09 18:23:31 +0530

    [MU] Common Button added

--- src/components/Categories/SalonCategoryCard.tsx
+++ src/components/Categories/SalonCategoryCard.tsx
@@ -22,6 +22,8 @@ import CategoryEditModal from "./CategoryEditModal";
 import DeleteModal from "../common/DeleteModal/DeleteModal";
 import { SalonBestOffersData } from "../../utils/data/SalonPatnerBestOffers/SalonBestOffers";
 import SalonBestOffersModel from "../OffersSection/SalonBestOffersModel";
+import EditButton from "../common/CommonButtons/EditButton";
+import DeleteButton from "../common/CommonButtons/DeleteButton";
 
 interface categoryProps {
   classes: any;
@@ -44,52 +46,55 @@ class SalonCategoryCard extends Component<categoryProps> {
     this.setState({ open: true });
   };
   onEditClick = () => {
-    this.setState({ edit: true })
-  }
+    this.setState({ edit: true });
+  };
   render() {
     const { classes } = this.props;
     return (
       <>
         <Grid container spacing={4} className={classes.categoryImageBox}>
-            {salonCategoryData.map((item: any) => {
-                return (
-                <Grid item lg={4} md={4} sm={6} xs={12} key={item.id}>
-                    <Card className={classes.categoryCard}>
-                      <CardActionArea>
-                        <CardMedia
-                          className={classes.categoryImage}
-                          image={item.img}
-                          title="categoryImg"
-                        />
-                        <CardContent className={classes.categoryContent}>
-                          <Typography>{item.title}</Typography>
-                        </CardContent>
-                        <CardActions className={classes.categoryButtonBox}>
-                          <Button onClick={this.onEditClick}>
-                              <EditIcon />
-                              {item.editButton}
-                          </Button>
-                          <Button onClick={this.onDeleteClick}>
-                              <DeleteIcon />
-                              {item.deleteButton}
-                          </Button>
-                        </CardActions>
-                      </CardActionArea>
-                    </Card>
-                </Grid>
-                );
-            })}
+          {salonCategoryData.map((item: any) => {
+            return (
+              <Grid item lg={4} md={4} sm={6} xs={12} key={item.id}>
+                <Card className={classes.categoryCard}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.categoryImage}
+                      image={item.img}
+                      title="categoryImg"
+                    />
+                    <CardContent className={classes.categoryContent}>
+                      <Typography>{item.title}</Typography>
+                    </CardContent>
+                    <CardActions
+                      className={classes.categoryButtonBox}
+                    >
+                      <EditButton
+                        className=""
+                        buttonName="Edit"
+                        icon={<EditIcon />}
+                        onClick={this.onEditClick}
+                      />
+                      <DeleteButton
+                        className=""
+                        buttonName="Delete"
+                        icon={<DeleteIcon />}
+                        onClick={this.onDeleteClick}
+                      />
+                      
+                    </CardActions>
+                  </CardActionArea>
+                </Card>
+              </Grid>
+            );
+          })}
         </Grid>
         <DeleteModal
           jobTitle="Category"
           open={this.state.open}
           onClose={this.onDeleteClose}
         />
-        <CategoryEditModal
-          open={this.state.edit}
-          onClose={this.onEditClose}
-        />
-        
+        <CategoryEditModal open={this.state.edit} onClose={this.onEditClose} />
       </>
     );
   }
---
+++ src/components/common/CommonButtons/EditButton.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@mui/material";
+
+import React, { Component, Fragment } from "react";
+import { withStyles } from "@mui/styles";
+
+import { CommonButtonEditDelete } from "./buttons.Styles";
+
+interface IsNameProps {
+  classes: any;
+  buttonName: string;
+  onClick: (id?: any) => void;
+  icon?: React.ReactNode;
+  className:any;
+
+}
+
+export class EditButton extends Component<IsNameProps> {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <>
+       
+          <Button
+            className={classes.EditButton}
+            onClick={this.props.onClick}
+            startIcon={this.props.icon}
+            sx={{
+              fontSize: { xs: "16px", sm: "18px", lg: "18px" },
+            }}
+          >
+            {this.props.icon}
+            {this.props.buttonName}
+          </Button>
+      
+      </>
+    );
+  }
+}
+
+export default withStyles(CommonButtonEditDelete)(EditButton);
