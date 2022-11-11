6913ee8c454bd9b8bf85ed1bb2e40c732b611a99
kiranrega <kiran.minpro@gmail.com>
2022-11-11 17:35:55 +0530

    [KR]-updated

--- src/components/SalonNotification/SalonNotification.style.ts
+++ src/components/SalonNotification/SalonNotification.style.ts
@@ -1,30 +1,37 @@
 /** @format */
+import theme from "../../Theme/Theme";
 
 export const NotificationStyles: any = {
   MainDialogBox: {
     // width: "395px",
-    height: "763px",
+    // height: "763px",
     "& .css-yiavyu-MuiBackdrop-root-MuiDialog-backdrop": {
       backgroundColor: "transparent",
     },
-    "& .css-1t1j96h-MuiPaper-root-MuiDialog-paper": {
+    "& .MuiDialog-paper": {
       position: "fixed",
       top: "43px",
       right: "10px",
       width: "390px",
       height: "600px",
       overflowX: "hidden",
-
       background: "#fff !important",
       boxShadow: "4px 4px 50px rgba(0, 0, 0, 0.15) !important",
       borderRadius: " 0px 0px 5px 5px",
 
-      "@media only screen and (max-width: 660px) ": {
+      [theme.breakpoints.down("sm")]: {
         position: "fixed",
-        top: "45px",
-        right: "-32px",
+        top: "148px",
         width: "100%",
         height: "766px",
+        margin: "0px !important",
+        right: "0px !important",
+      },
+      [theme.breakpoints.down("md")]: {
+        position: "fixed",
+        top: "148px",
+        margin: "0px !important",
+        right: "0px !important",
       },
     },
   },
@@ -44,9 +51,9 @@ export const NotificationStyles: any = {
     },
   },
   listItemNotification: {
-    "& .css-lxljjk-MuiListItem-root": {
-      padding: "0px",
-    },
+    // "& .MuiListItem-root": {
+    //   padding: "0px",
+    // },
   },
   listItemNotificationText: {
     marginLeft: "10px",
